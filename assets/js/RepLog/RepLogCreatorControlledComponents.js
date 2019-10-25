24832a67fcb33f41e690747720a265e166af0828
Andres Lara <andreslara@ipglobal.es>
2019-10-25 14:20:10 +0200

    More components

--- assets/js/RepLog/RepLogCreatorControlledComponents.js
+++ assets/js/RepLog/RepLogCreatorControlledComponents.js
@@ -1,31 +1,40 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 
-import Button from "../Components/Button";
-
 export default class RepLogCreator extends Component {
     constructor(props) {
         super(props);
 
         this.state = {
+            selectedItemId: '',
+            quantityValue: 0,
             quantityInputError: ''
         };
 
-        this.quantityInput = React.createRef();
-        this.itemSelect = React.createRef();
+        this.itemOption = [
+            { id: 'cat', text: 'Cat' },
+            { id: 'fat_cat', text: 'Big Fat Cat' },
+            { id: 'laptop', text: 'My Laptop' },
+            { id: 'coffee_cup', text: 'Coffee Cup' }
+        ];
 
         this.handleFormSubmit = this.handleFormSubmit.bind(this);
+        this.handleSelectedItemChange = this.handleSelectedItemChange.bind(this);
+        this.handleQuantityInputChange = this.handleQuantityInputChange.bind(this);
     }
 
     handleFormSubmit(event) {
         event.preventDefault();
 
         const { onAddRepLog } = this.props;
+        const { selectedItemId, quantityValue } = this.state;
 
-        const quantityInput = this.quantityInput.current;
-        const itemSelect = this.itemSelect.current;
 
-        if(quantityInput.value <= 0) {
+        const itemLabel = this.itemOption.find((option) => {
+            return option.id === selectedItemId;
+        }).text;
+
+        if(quantityValue.value <= 0) {
             this.setState({
                 quantityInputError: 'Please enter a value greater than 0'
             });
@@ -34,37 +43,44 @@ export default class RepLogCreator extends Component {
         }
 
         onAddRepLog(
-            itemSelect.options[itemSelect.selectedIndex].value,
-            quantityInput.value
+            itemLabel,
+            quantityValue
         );
 
-        quantityInput.value = '';
-        itemSelect.selectedIndex = 0;
         this.setState({
             quantityInputError: ''
         });
     }
 
+    handleSelectedItemChange(event) {
+        this.setState({
+            selectedItemId: event.target.value
+        })
+    }
+
+    handleQuantityInputChange(event) {
+        this.setState({
+            quantityValue: event.target.value
+        })
+    }
+
     render() {
-        const { quantityInputError } = this.state;
-        const { validationErrorMessage, itemOptions } = this.props;
+        const { quantityInputError, selectedItemId, quantityValue } = this.state;
 
         return (
             <form onSubmit={this.handleFormSubmit}>
-                {validationErrorMessage && (
-                    <div className="alert alert-danger">{validationErrorMessage}</div>
-                )}
                 <div className="form-group">
                     <label className="sr-only control-label required"
                            htmlFor="rep_log_item">
                         What did you lift?
                     </label>
                     <select id="rep_log_item"
-                            ref={this.itemSelect}
+                            value={selectedItemId}
+                            onChange={this.handleSelectedItemChange}
                             required="required"
                             className="form-control">
                         <option value="">What did you lift?</option>
-                        {itemOptions.map(option => {
+                        {this.itemOption.map(option => {
                             return <option value={option.id} key={option.id}>{option.text}</option>
                         })}
                     </select>
@@ -76,17 +92,15 @@ export default class RepLogCreator extends Component {
                     </label>
                     <input type="number"
                            id="rep_log_reps"
-                           ref={this.quantityInput}
+                           value={quantityValue}
+                           onChange={this.handleQuantityInputChange}
                            required="required"
                            placeholder="How many times?"
                            className="form-control"/>
                     {quantityInputError && <span className={"help-block"}>{quantityInputError}</span>}
                 </div>
                 {' '}
-                <Button type="submit" className="btn-primary">
-                    I Lifted it!
-                    <span className="fa fa-plus-circle"></span>
-                </Button>
+                <button type="submit" className="btn btn-primary">I Lifted it!</button>
             </form>
         );
     }
@@ -94,6 +108,4 @@ export default class RepLogCreator extends Component {
 
 RepLogCreator.propTypes = {
     onAddRepLog: PropTypes.func.isRequired,
-    validationErrorMessage: PropTypes.string.isRequired,
-    itemOptions: PropTypes.array.isRequired,
 };
